589868675715861fc2daec9441f8d36e2d23702c
volotinaiaiu <yanavolotina111@mail.ru>
2023-12-28 16:15:39 +0300

    Фикс ошибок

--- Volotina_Repository_v3.cpp
+++ Volotina_Repository_v3.cpp
@@ -11,7 +11,7 @@ int inputA() {
 
 int inputB() {
 	int b;
-	cout << "Введите число В: ";
+	cout << "Введите число B: ";
 	cin >> b;
 	return b;
 }
@@ -21,52 +21,44 @@ int Diff(int A, int B) {
 }
 
 int Sum(int A, int B) {
-	return (A + B);
+	int sum = A + B;
+	return sum;
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 	int function, A = 0, B = 0;
-	cout << "1 - Ввести число А\n2 - Ввести число В\n3 - Выполнить операцию сложение\n4 - Выполнить операцию вычитание\n";
-	cout << "\nВведите номер функции: ";
-	cin >> function;
-
-	switch (function)
+	while (true)
 	{
-		case 1:
-		{
-			A = inputA();
-			break;
-		}
-		case 2:
-		{
-			B = inputB();
-			break;
-		}
-		case 3:
-		{
-			if (!(A & B))
-			{
-				cout << "\nСначала надо ввести числа!\n\n";
-				break;
-			}
-			cout << "Сумма A и B: " << Sum(A, B) << endl;
-			break;
-		}
-		case 4:
-		{
-			if (!(A & B))
-			{
-				cout << "\nСначала надо ввести числа!\n\n";
-				break;
-			}
-			cout << "Разность A и B: " << Diff(A, B) << endl;
-			break;
-		}
-		default:
-			break;
-	}
+		cout << "1 - Ввести число А\n2 - Ввести число В\n3 - Выполнить операцию сложение\n4 - Выполнить операцию вычитание\n";
+		cout << "\nВведите номер функции: ";
+		cin >> function;
 
-	main();
+		switch (function)
+		{
+			case 1:
+			{
+				A = inputA();
+				break;
+			}
+			case 2:
+			{
+				B = inputB();
+				break;
+			}
+			case 3:
+			{
+				cout << "Сумма A и B: " << Sum(A, B) << endl;
+				break;
+			}
+			case 4:
+			{
+				cout << "Разность A и B: " << Diff(A, B) << endl;
+				break;
+			}
+			default:
+				break;
+		}
+	}
 }

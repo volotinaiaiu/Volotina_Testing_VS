84e70b18a2145bd37815f23ced5201887710a864
volotinaiaiu <yanavolotina111@mail.ru>
2023-12-28 15:36:23 +0300

    добавлены функции без описания

--- Volotina_Repository_v3.cpp
+++ Volotina_Repository_v3.cpp
@@ -2,34 +2,62 @@
 
 using namespace std;
 
+int inputA() {
+}
+
+int inputB() {
+}
+
+int Sum(int A, int B) {
+}
+
+int Diff(int A, int B) {
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-	int function;
+	int function, A = 0, B = 0;
 	cout << "1 - Ввести число А\n2 - Ввести число В\n3 - Выполнить операцию сложение\n4 - Выполнить операцию вычитание\n";
-	cout << "Введите номер функции:\n";
+	cout << "\nВведите номер функции: ";
 	cin >> function;
 
 	switch (function)
 	{
 		case 1:
 		{
+			A = inputA();
 			break;
 		}
 		case 2:
 		{
+			B = inputB();
 			break;
 		}
 		case 3:
 		{
+			if (!(A & B))
+			{
+				cout << "\nСначала надо ввести числа!\n\n";
+				break;
+			}
+			cout << "Сумма A и B: " << Sum(A, B) << endl;
 			break;
 		}
 		case 4:
 		{
+			if (!(A & B))
+			{
+				cout << "\nСначала надо ввести числа!\n\n";
+				break;
+			}
+			cout << "Разность A и B: " << Diff(A, B) << endl;
 			break;
 		}
 		default:
 			break;
 	}
+
+	main();
 }

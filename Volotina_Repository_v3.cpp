c932fb73a0c686f225ba207fc23efcc08c7860d1
volotinaiaiu <yanavolotina111@mail.ru>
2023-12-28 15:28:00 +0300

    1 функция

--- Volotina_Repository_v3.cpp
+++ Volotina_Repository_v3.cpp
@@ -2,34 +2,66 @@
 
 using namespace std;
 
+int inputA() {
+	int a;
+	cout << "Введите число А: ";
+	cin >> a;
+	return a;
+}
+
+int inputB() {
+}
+
+int Sum(int A, int B) {
+}
+
+int Diff(int A, int B) {
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-	int function;
+	int function, A = 0, B = 0;
 	cout << "1 - Ввести число А\n2 - Ввести число В\n3 - Выполнить операцию сложение\n4 - Выполнить операцию вычитание\n";
-	cout << "Введите номер функции:\n";
+	cout << "\nВведите номер функции: ";
 	cin >> function;
 
 	switch (function)
 	{
 		case 1:
 		{
+			A = inputA();
 			break;
 		}
 		case 2:
 		{
+			B = inputB();
 			break;
 		}
 		case 3:
 		{
+			if (!(A & B))
+			{
+				cout << "\nСначала надо ввести числа!\n\n";
+				break;
+			}
+			Sum(A, B);
 			break;
 		}
 		case 4:
 		{
+			if (!(A & B))
+			{
+				cout << "\nСначала надо ввести числа!\n\n";
+				break;
+			}
+			Diff(A, B);
 			break;
 		}
 		default:
 			break;
 	}
+
+	main();
 }

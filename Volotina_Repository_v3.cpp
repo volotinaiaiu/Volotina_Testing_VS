14bd0157a62eabffb9fbdcd09368eead9c90de25
volotinaiaiu <yanavolotina111@mail.ru>
2023-12-28 14:57:05 +0300

    Меню выбора

---
+++ Volotina_Repository_v3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+	int function;
+	cout << "1 - Ввести число А\n2 - Ввести число В\n3 - Выполнить операцию сложение\n4 - Выполнить операцию вычитание\n";
+	cout << "Введите номер функции:\n";
+	cin >> function;
+
+	switch (function)
+	{
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+		default:
+			break;
+	}
+}
